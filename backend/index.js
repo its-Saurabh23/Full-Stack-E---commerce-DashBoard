a6fe80ec624e6b4669403eb11a437d16a4952a79
its-Saurabh23 <kavinelevin12345@gmail.com>
2023-02-04 16:44:03 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -74,6 +74,18 @@ app.put("/product/:id",async(req,resp)=>{
     resp.send(result);
 });
 
+
+app.get("/search/:key",async(req,resp)=>{
+let result = await Product.find({
+  "$or":[
+    { name:{$regex:req.params.key}},
+    { company:{$regex:req.params.key}},
+    { category:{$regex:req.params.key}}
+  ]
+});
+resp.send(result);
+})
+
 app.listen(5000, () => console.log("port runing on 5000"));
 
 
--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -20,13 +20,28 @@ let result =  await fetch(`http://localhost:5000/product/${id}`,{
   if (result) {
     alert("record is deleted");
     getProducts();
-
-  }
+ }
 }
+const searchHandle = async(e)=>{
+    // console.log(e.target.value)
+    let key = e.target.value;
+    if (key) {
+        let result = await fetch(`http://localhost:5000/search/${key}`);
+        result = await result.json();
+        if (result) {
+       setProducts(result);
+        }
+    }
+    else{
+        getProducts();
+    }
+  }
 
     return(
         <div className='products-list'>
             <h3>Priduct List</h3>
+            <input type="text" className='search-product-box'  placeholder='Search Product'
+            onChange={searchHandle}/>
             <ul>
                 <li>S. No</li>
                 <li>Name</li>
@@ -35,19 +50,18 @@ let result =  await fetch(`http://localhost:5000/product/${id}`,{
                 <li>Operation</li>
             </ul>
             {
-                products.map((item,index)=>
+            products.length>0 ? products.map((item,index)=>
                     <ul key={item._id }>
                     <li>{index+1}</li>
                     <li>{item.name}</li>
                     <li> $ {item.price}</li>
                     <li>{item.category}</li>
                     <li><button onClick={()=>deleteProduct(item._id)}>Delete</button>
-                    <Link to={"/update/"+ item._id}>Update </Link>
-                    
+                    <Link to={"/update/"+ item._id}>Update </Link> 
                     </li>
-
                 </ul>
                 )
+                :<h1>No Result Found</h1>
             }
         </div>
     )
