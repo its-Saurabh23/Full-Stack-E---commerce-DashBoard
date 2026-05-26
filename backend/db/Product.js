020f217e6a4bab95cfd9c506c9ad3de83d8aee4d
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2026-05-26 22:09:23 +0530

    Update Product.js remove unwanted space

--- backend/db/Product.js
+++ backend/db/Product.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
-
     name:String,
     price:String,
     category:String,
     userId:String,
     company:String
+
 },{
     versionKey:false
 });
